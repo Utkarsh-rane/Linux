1d0cf2096246b3b8074e84def23e38938143e84b
Utkarsh-rane <130978240+Utkarsh-rane@users.noreply.github.com>
2023-04-20 15:40:41 +0530

    Update 40_assi.sh

--- programs/40_assi.sh
+++ programs/40_assi.sh
@@ -1,1 +1,18 @@
-
+#!/bin/bash
+read -p "enter number of rows: "n
+for((i=1; i<=n; i++))
+do
+    for((j=1; j<n-1; j++))
+    do
+       echo -n " "
+    done
+    for((k=1; k<=i; k++))
+    do
+        printf "%s " "$(printf '%c' $((k+64)))"
+    done
+    for ((l=i-1; l>=1; l--))
+    do
+        printf "%s " "$(printf '%c' $((l+64)))"
+    done
+    echo ""
+done
